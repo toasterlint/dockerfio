40b308a24acc5511c40eb8e232c5a90c49287d46
Henry Mohn <hmohniii@gmail.com>
2018-03-01 23:52:09 -0600

    Adding different types of tests

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # dockerfio
 FIO Testing in a docker container
+
+To run read test:
+docker run -it -v /path/to/test/dir:/data -e JOB_NAME='read' toasterlint/fio
+
+To run R(60%) / W(40%) test:
+docker run -it -v /path/to/test/dir:/data -e JOB_NAME='rw' toasterlint/fio
--- start.sh
+++ start.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
 #set unless otherwise passed into the container
-export FIO_JOB_FILE=${FIO_JOB_FILE:-/config/job.fio}
-export JOB_NAME=${JOB_NAME:-fio}
-export NJ=${NJ:-8}
-export QD=${QD:-16}
-export BLOCK_SIZE=${BLOCK_SIZE:-4k}
-export DEV=${DEV:-/data/testfile}
-export DIRECT=${DIRECT:-1}
-export SIZE=${SIZE:-5g}
-export RW=${RW:-read}
-export RT=${RT:-300}
+export JOB_NAME=${JOB_NAME:-read}
 
-exec /usr/bin/fio $FIO_JOB_FILE --output /data/${JOB_NAME}.out
+exec /usr/bin/fio ${JOB_NAME}job.fio --output /data/${JOB_NAME}.out
