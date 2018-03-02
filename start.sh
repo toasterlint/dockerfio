cf16cf61f7582d3e0528bd2b0814918e8b65944d
Henry Mohn <hmohniii@gmail.com>
2018-03-02 00:01:11 -0600

    Fixed start.sh to look for configs in /config

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 #set unless otherwise passed into the container
 export JOB_NAME=${JOB_NAME:-read}
 
-exec /usr/bin/fio ${JOB_NAME}job.fio --output /data/${JOB_NAME}.out
+exec /usr/bin/fio /config/${JOB_NAME}job.fio --output /data/${JOB_NAME}.out
